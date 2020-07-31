af72f325d1cf14054612fc3beb3af1784ce7ef02
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-31 21:22:22 +0200

    implemented game functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import './components/footer';
-
 import './sass/index.scss';
+import { shuffleCards } from './components/Cards';
+import { Players } from './components/Players';
+import { Deck as IDeck } from './interfaces/Deck';
 
-import { form } from './forms';
-console.log(form);
+const startBtn = document.getElementById('btn__start')!;
+const hitBtn = document.getElementById('btn__hit')!;
+const stayBtn = document.getElementById('btn__stay')!;
 
-const newfunc = (name: string) => console.log(name);
+let pile: IDeck;
+let currentPlayer = 0;
+const players = new Players();
 
-newfunc('asia');
+const setDeck = async () => {
+	const deck = await shuffleCards().then((resp) => resp);
+	pile = await deck;
+	return pile;
+};
+
+const startGame = async (_e: Event) => {
+	document.getElementById('btn__start')!.innerHTML = 'Restart';
+	document.getElementById('status')!.style.display = 'none';
+	await setDeck();
+	currentPlayer = 0;
+	players.createPlayers(2);
+	players.createPlayersUI();
+	players.dealHands(pile.deck_id);
+
+	document.getElementById(`player_${currentPlayer}`)!.classList.add('active');
+};
+
+startBtn.addEventListener('click', startGame);
+
+hitBtn.addEventListener('click', () => {
+	console.log('hit me');
+});
+stayBtn.addEventListener('click', () => {
+	console.log('stay');
+});
