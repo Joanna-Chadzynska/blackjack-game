b6fb54c6916d0bf8db3ae7cfd1a7ac2f70b379ea
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-03 13:30:31 +0200

    code refractoring

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import './components/footer';
 import './sass/index.scss';
 import { shuffleCards } from './components/Cards';
-import { Players } from './components/Players';
+import { Game } from './components/Game';
 import { Deck as IDeck } from './interfaces/Deck';
 
 const startBtn = document.getElementById('btn__start')!;
 const hitBtn = document.getElementById('btn__hit')!;
 const stayBtn = document.getElementById('btn__stay')!;
+// const deckCount = document.getElementById('deckCount')!;
 
 let pile: IDeck;
-let currentPlayer = 0;
-const players = new Players();
+
+const game = new Game();
 
 const setDeck = async () => {
 	const deck = await shuffleCards().then((resp) => resp);
@@ -18,23 +19,29 @@ const setDeck = async () => {
 	return pile;
 };
 
+setDeck();
+
 const startGame = async (_e: Event) => {
 	document.getElementById('btn__start')!.innerHTML = 'Restart';
 	document.getElementById('status')!.style.display = 'none';
-	await setDeck();
-	currentPlayer = 0;
-	players.createPlayers(2);
-	players.createPlayersUI();
-	players.dealHands(pile.deck_id);
 
-	document.getElementById(`player_${currentPlayer}`)!.classList.add('active');
+	game.currentPlayer;
+	game.createPlayers(2);
+	game.createPlayersUI();
+	game.dealHands(pile.deck_id);
+
+	document
+		.getElementById(`player_${game.currentPlayer}`)!
+		.classList.add('active');
 };
 
 startBtn.addEventListener('click', startGame);
 
 hitBtn.addEventListener('click', () => {
 	console.log('hit me');
+	game.hitMe(pile.deck_id);
 });
+
 stayBtn.addEventListener('click', () => {
 	console.log('stay');
 });
