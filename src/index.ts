266f1c96985ac4540ebef9fca05927dc397dd187
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-04 20:02:38 +0200

    implemented select mode functionality

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,25 @@ import './sass/index.scss';
 import { Game } from './components/Game';
 
 const startBtn = document.getElementById('btn__start')!;
+const modeBtns = [
+	...document.querySelectorAll<HTMLButtonElement>('.btn__mode')!,
+];
+const selectedMode = document.querySelector('.game__selected-mode')!;
 
-const startGame = () => {
-	let game = new Game();
-	game.startGame();
+const startGame = (players: number) => {
+	const game = new Game();
+	game.startGame(players);
 };
 
-startBtn.addEventListener('click', startGame);
+const selectMode = (e: Event) => {
+	const mode = (<HTMLButtonElement>e.currentTarget).dataset.mode;
+	const modeName = (<HTMLButtonElement>e.currentTarget).innerText;
+	selectedMode.classList.add('show');
+
+	document.getElementById(
+		'mode__info'
+	)!.innerHTML = `Selected mode: ${modeName}`;
+	startBtn.addEventListener('click', () => startGame(Number(mode)));
+};
+
+modeBtns.forEach((mode) => mode.addEventListener('click', selectMode));
