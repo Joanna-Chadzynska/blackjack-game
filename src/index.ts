f862500fe06d2aaa459891269b732a28ff231739
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-05 22:34:14 +0200

    add localStorage for players: problem with change mode fixed

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,24 @@ const selectedMode = document.querySelector('.game__selected-mode')!;
 
 const startGame = (players: number) => {
 	const game = new Game();
+	// set number of players in local storage
+	localStorage.setItem('players', players.toString());
 	game.startGame(players);
 };
 
 const selectMode = (e: Event) => {
 	const mode = (<HTMLButtonElement>e.currentTarget).dataset.mode;
 	const modeName = (<HTMLButtonElement>e.currentTarget).innerText;
+
+	// remove data from local storage before game initialization
+	localStorage.removeItem('players');
+
 	selectedMode.classList.add('show');
 
 	document.getElementById(
 		'mode__info'
 	)!.innerHTML = `Selected mode: ${modeName}`;
+
 	startBtn.addEventListener('click', () => startGame(Number(mode)));
 };
 
